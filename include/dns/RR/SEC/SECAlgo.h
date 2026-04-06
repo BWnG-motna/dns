d7b46119e4e54ff25af44bc31a9b43255f09c8fa
Daniel Lee <yukin@yukin.pl>
2026-04-06 17:46:49 +0900

    error correction related SEC algorithm ( reference : https://www.iana.org/assignments/dns-sec-alg-numbers )

--- include/dns/RR/SEC/SECAlgo.h
+++ include/dns/RR/SEC/SECAlgo.h
@@ -20,16 +20,19 @@ enum class SECAlgo : uint8_t
 {
 	INVALID                  =   0 ,
 	RSA_MD5                  =   1 , // RFC 8624 - MUST NOT      / 128 bytes
-	DSA_SHA1                 =   2 , // RFC 8624 - MUST NOT      /  64 bytes
+	DH                       =   2 , // RFC 8624 - MUST NOT
+	DSA_SHA1                 =   3 , // RFC 8624 - MUST NOT      /  64 bytes
 	RSA_SHA1                 =   5 , // RFC 8624 - SHOULD NOT    / 128 bytes
+	DSA_NSEC3_SHA1           =   6 , // RFC 8624 - MUST NOT
 	RSA_SHA1_NSEC3_SHA1      =   7 , // RFC 8624 - SHOULD NOT    / 128 bytes
 	RSA_SHA256               =   8 , // RFC 8624 - MUST          / 256 bytes
 	RSA_SHA512               =  10 , // RFC 8624 - SHOULD        / 512 bytes
-	GOST_R_34_10_2011        =  12 , // RFC 8624 - NOT RECOMMAND / ?
+	GOST_R_34_10_2001        =  12 , // RFC 8624 - NOT RECOMMAND --> RFC 9906 DEPRECATED
 	ECDSA_CURVE_P256_SHA256  =  13 , // RFC 8624 - MUST          /  64 bytes
 	ECDSA_CURVE_P384_SHA384  =  14 , // RFC 8624 - SHOULD        /  86 bytes
 	ED25519                  =  15 , // RFC 8624 - MUST          /  64 bytes
 	ED448                    =  16 , // RFC 8624 - SHOULD        /  96 bytes
+	GOST_R_34_10_2012        =  23 , // RFC 8624 - MUST NOT
 } ;
 
 
