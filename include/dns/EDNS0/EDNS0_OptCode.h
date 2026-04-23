3b83f1402fe39a0cc0f8b90508b28518c403572c
Daniel Lee <yukin@yukin.pl>
2026-04-23 14:44:03 +0900

    edns0 option code's description added

--- include/dns/EDNS0/EDNS0_OptCode.h
+++ include/dns/EDNS0/EDNS0_OptCode.h
@@ -14,27 +14,29 @@ namespace dns
 enum class EDNS0_OptCode : uint16_t
 {
 	RESERVED        =      0 ,
-	LLQ             =      1 ,
+	LLQ             =      1 , // Long Lived Queries
 	UL              =      2 , // Update Lease
-	NSID            =      3 ,
+	NSID            =      3 , // Name Server Identifier
 
-	DAU             =      5 ,
-	DHU             =      6 ,
-	N3U             =      7 ,
-	CLINET_SUBNET   =      8 ,
-	EXPIRE          =      9 ,
-	COOKIE          =     10 ,
-	TCP_KEEPALIVE   =     11 ,
-	PADDING         =     12 ,
-	CHAIN           =     13 ,
-	KEYTAG          =     14 ,
-	EDE             =     15 ,  // Extended DNS Error
+	DAU             =      5 , // DNSSEC algorithm understood
+	DHU             =      6 , // DS Hash Understood
+	N3U             =      7 , // NSEC3 Hash Understood
+	ECS             =      8 , // edns-client-subnet ( ECS ) 
+	EDNS_EXPIRE     =      9 , // EDNS EXPIRE ( RFC 7314 )
+	COOKIE          =     10 , // COOKIE ( RFC 7873 )
+	TCP_KEEPALIVE   =     11 , // edns-tcp-Keepalive ( RFC 7828 )
+	PADDING         =     12 , // Padding ( RFC 7830 )
+	CHAIN           =     13 , // CHAIN ( RFC 7901 )
+	EDNS_KEY_TAG    =     14 , // ends-key-tag ( RFC 8145 )
+	EDE             =     15 , // Extended DNS Error ( RFC 8914 )
 
-	REPORT_CHANNEL  =     18 ,
-	ZONEVERSION     =     19 ,
+	REPORT_CHANNEL  =     18 , // Report-Channel ( RFC 9567 )
+	ZONEVERSION     =     19 , // ZONEVERSION ( RFC 9660 )
 
-	UmbrellaIdent   =  20292 ,
-	DeviceID        =  26946 ,
+	UmbrellaIdent   =  20292 , // Umbrella Ident
+	DeviceID        =  26946 , // Device ID
+
+	ReservedFuture  =  65535 ,
 
 
 } ; // enum class EDNS0_OptCode
--- src/dns/EDNS0/EDNS0_OptCode.cpp
+++ src/dns/EDNS0/EDNS0_OptCode.cpp
@@ -5,30 +5,30 @@ bool daniel::dns::IsValidEDNS0_OptCode( uint16_t const & codeNo )
 {
 	switch( codeNo ) 
 	{
-		case 1 :
-		case 2 :
-		case 3 :
+		case     1 : // LLQ             =      1 , // Long Lived Queries
+		case     2 : // UL              =      2 , // Update Lease
+		case     3 : // NSID            =      3 , // Name Server Identifier
 			return true ;
 
-		case 5 :
-		case 6 :
-		case 7 :
-		case 8 :
-		case 9 :
-		case 10 :
-		case 11 :
-		case 12 :
-		case 13 :
-		case 14 :
-		case 15 :
+		case     5 : // DAU             =      5 , // DNSSEC algorithm understood
+		case     6 : // DHU             =      6 , // DS Hash Understood
+		case     7 : // N3U             =      7 , // NSEC3 Hash Understood
+		case     8 : // ECS             =      8 , // edns-client-subnet ( ECS ) 
+		case     9 : // EDNS_EXPIRE     =      9 , // EDNS EXPIRE ( RFC 7314 )
+		case    10 : // COOKIE          =     10 , // COOKIE ( RFC 7873 )
+		case    11 : // TCP_KEEPALIVE   =     11 , // edns-tcp-Keepalive ( RFC 7828 )
+		case    12 : // PADDING         =     12 , // Padding ( RFC 7830 )
+		case    13 : // CHAIN           =     13 , // CHAIN ( RFC 7901 )
+		case    14 : // EDNS_KEY_TAG    =     14 , // ends-key-tag ( RFC 8145 )
+		case    15 : // EDE             =     15 , // Extended DNS Error ( RFC 8914 )
 			return true ;
 
-		case 18 :
-		case 19 :
+		case    18 : // REPORT_CHANNEL  =     18 , // Report-Channel ( RFC 9567 )
+		case    19 : // ZONEVERSION     =     19 , // ZONEVERSION ( RFC 9660 )
 			return true ;
 
-		case 20292 :
-		case 26946 :
+		case 20292 : // UmbrellaIdent   =  20292 , // Umbrella Ident
+		case 26946 : // DeviceID        =  26946 , // Device ID
 			return false ; 
 
 		default :
